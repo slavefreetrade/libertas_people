f72c0fedd8160326895754d27fd2e2db7a76965a
fabuloussTeam <tjomemmanuel@gmail.com>
2020-08-24 18:30:09 +0100

    work done: Emmanuel's page create and Tile

--- lib/ui/widgets/our_drawer.dart
+++ lib/ui/widgets/our_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:libertaspeople/ui/pages/emmanuels_page.dart';
 import 'package:libertaspeople/ui/pages/home_page.dart';
 import 'package:libertaspeople/ui/pages/peters_page.dart';
 
@@ -29,6 +30,16 @@ class OurDrawer extends StatelessWidget {
               Navigator.of(context).push(
                 MaterialPageRoute(
                   builder: (context) => PetersPage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: Text("Emmanuel's Page"),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => EmmanuelsPage(),
                 ),
               );
             },
