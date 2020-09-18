b401cac8c529f37f6c7dbc07d454153968e5773d
12827629 <michael.k.malak@gmail.com>
2020-09-18 07:58:00 +0200

    Adjusting about page colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -9,13 +9,14 @@ class ColorConstants {
   static const Color white = const Color(0xFFFFFFFF);
 
   ///About Page
-  static const Color orangeAboutPage = const Color(0xFF6874F3);
-  static const Color greyAboutPage = const Color(0xFFDFDFE2);
-  static const Color lightGreenAboutPage = const Color(0xFF5DC5A9);
-  static const Color lightOrangishGreenAboutPage = const Color(0xFF2EB0CA);
-  static const Color darkOrangishGreenAboutPage = const Color(0xFF25AADF);
-  static const Color vibrantOrangeAboutPage = const Color(0xFF286DDB);
-  static const Color blueAboutPage = const Color(0xFFC1A754);
-  static const Color purpleAboutPage = const Color(0xFFA86E55);
-  static const Color darkBrownAboutPage = const Color(0xFF286DDB);
+  static const Color orangeAboutPage = const Color(0xFFF37468);
+  static const Color greyAboutPage = const Color(0xFFe2dfdf);
+  static const Color lightGreenAboutPage = const Color(0xFFa9c55d);
+  static const Color darkGreenAboutPage = const Color(0xFF538764);
+  static const Color lightOrangishGreenAboutPage = const Color(0xFFcab02e);
+  static const Color darkOrangishGreenAboutPage = const Color(0xFFdfaa25);
+  static const Color vibrantOrangeAboutPage = const Color(0xFFdb6d28);
+  static const Color blueAboutPage = const Color(0xFF54a7c1);
+  static const Color purpleAboutPage = const Color(0xFF556ea8);
+  static const Color darkBrownAboutPage = const Color(0xFF4b292f);
 }
