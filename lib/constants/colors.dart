8998d4d412c169d52e5e4756b79e48b45ad2317a
12827629 <michael.k.malak@gmail.com>
2020-09-25 17:44:19 +0200

    Add shared buttons

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ColorConstants {
-  static const Color darkBlue = const Color(0xFF1A4074);
-  static const Color lightBlue = const Color(0xFF2CAAE2);
-  static const Color veryLightBlue = const Color(0xFFF2F2F2);
-  static const Color orange = const Color(0xFFF37468);
-  static const Color black = const Color(0xFF000000);
-  static const Color white = const Color(0xFFFFFFFF);
+  static const Color darkBlue = Color(0xff1a4074);
+  static const Color lightBlue = Color(0xff2caae2);
+  static const Color veryLightBlue = Color(0xfff2f2f2);
+  static const Color orange = Color(0xfff37468);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffffffff);
+  static const Color backgroundColor = Color(0xfff8f8f8);
 
   ///About Page
-  static const Color orangeAboutPage = const Color(0xFFF37468);
-  static const Color greyAboutPage = const Color(0xFFe2dfdf);
-  static const Color lightGreenAboutPage = const Color(0xFFa9c55d);
-  static const Color darkGreenAboutPage = const Color(0xFF538764);
-  static const Color lightOrangishGreenAboutPage = const Color(0xFFcab02e);
-  static const Color darkOrangishGreenAboutPage = const Color(0xFFdfaa25);
-  static const Color vibrantOrangeAboutPage = const Color(0xFFdb6d28);
-  static const Color blueAboutPage = const Color(0xFF54a7c1);
-  static const Color purpleAboutPage = const Color(0xFF556ea8);
-  static const Color darkBrownAboutPage = const Color(0xFF4b292f);
+  static const Color orangeAboutPage = Color(0xfff37468);
+  static const Color greyAboutPage = Color(0xffe2dfdf);
+  static const Color lightGreenAboutPage = Color(0xffa9c55d);
+  static const Color darkGreenAboutPage = Color(0xFF538764);
+  static const Color lightOrangishGreenAboutPage = Color(0xffcab02e);
+  static const Color darkOrangishGreenAboutPage = Color(0xffdfaa25);
+  static const Color vibrantOrangeAboutPage = Color(0xffdb6d28);
+  static const Color blueAboutPage = Color(0xff54a7c1);
+  static const Color purpleAboutPage = Color(0xff556ea8);
+  static const Color darkBrownAboutPage = Color(0xff4b292f);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,55 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'constants/colors.dart';
+import 'generated/l10n.dart';
 import 'pages/ui_development_page.dart';
 
+
 void main() {
-  runApp(MyApp());
+  runApp(
+   DevicePreview(
+     enabled: false, //!kReleaseMode,
+     builder: (context) =>
+          MyApp(),
+   ),
+  );
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Libertas People',
+      // ignore: prefer_const_literals_to_create_immutables
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        S.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       theme: ThemeData(
+        backgroundColor: ColorConstants.backgroundColor,
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        buttonTheme: const ButtonThemeData(
+          padding: EdgeInsets.zero,
+          height: 50,
+          minWidth: 30,
+          textTheme: ButtonTextTheme.primary,
+        ),
+        appBarTheme: const AppBarTheme(
+          color: ColorConstants.backgroundColor,
+          centerTitle: true,
+          elevation: 0,
+          textTheme: TextTheme(
+            headline6: TextStyle(
+                color: ColorConstants.orange,
+                fontSize: 32,
+                fontWeight: FontWeight.w700),
+          ),
+        ),
       ),
       home: UIDevelopmentPage(),
     );
