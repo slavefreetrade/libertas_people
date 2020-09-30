4ee22453c3037e7d52485cabe130a680034ef6b2
Souvik <souvik@pop-os.localdomain>
2020-09-30 17:54:26 +0530

    Made the user local class present in /lib/models/user_local_data_source.dart

--- lib/pages/ui_development_page.dart
+++ lib/pages/ui_development_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:libertaspeople/models/user_local_data_source.dart';
 
 class UIDevelopmentPage extends StatefulWidget {
   @override
@@ -9,103 +10,169 @@ class _UIDevelopmentPageState extends State<UIDevelopmentPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Home"),
-        ),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text("onboarding"),
-              onTap: () {
-                print("nav to onboarding 1 page");
-              },
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text("onboarding"),
+            onTap: () {
+              print("nav to onboarding 1 page");
+            },
+          ),
+          ListTile(
+            title: Text("create profile"),
+            onTap: () {
+              print("nav to create profile page");
+            },
+          ),
+          ListTile(
+            title: Text("feat-profile creation"),
+            onTap: () {
+              print("nav to workplace id page");
+            },
+          ),
+          ListTile(
+            title: Text("confirmation login_returning user"),
+            onTap: () {
+              print("nav to confirmation login_returning user page");
+            },
+          ),
+          ListTile(
+            title: Text("home_first time user"),
+            onTap: () {
+              print("nav to home_first time user page");
+            },
+          ),
+          ListTile(
+            title: Text("home_first no surveys"),
+            onTap: () {
+              print("nav to home_first no surveys page");
+            },
+          ),
+          ListTile(
+            title: Text("home_returning user"),
+            onTap: () {
+              print("nav to home_returning user page");
+            },
+          ),
+          ListTile(
+            title: Text("profile screen"),
+            onTap: () {
+              print("nav to profile screen page");
+            },
+          ),
+          ListTile(
+            title: Text("profile screen #5"),
+            onTap: () {
+              print("nav to profile screen #5 page");
+            },
+          ),
+          ListTile(
+            title: Text("survey language"),
+            onTap: () {
+              print("nav to survey language page");
+            },
+          ),
+          ListTile(
+            title: Text("survey information"),
+            onTap: () {
+              print("nav to survey information page");
+            },
+          ),
+          ListTile(
+            title: Text("survey Q1"),
+            onTap: () {
+              print("nav to survey Q1 page");
+            },
+          ),
+          ListTile(
+            title: Text("survey1 thank you"),
+            onTap: () {
+              print("nav to survey1 thank you page");
+            },
+          ),
+          ListTile(
+            title: Text("thank you screen retr=u"),
+            onTap: () {
+              print("nav to _ page");
+            },
+          ),
+          ListTile(
+            title: Text("about - before user fill survey"),
+            onTap: () {
+              print("nav to _ page");
+            },
+          ),
+          FlatButton(
+            onPressed: () {
+              var result = UserLocalDataSource().fetchUniqueDeviceID();
+              print("$result");
+            },
+            color: Colors.orange,
+            child: Text(
+              "Test device id",
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
-            ListTile(
-              title: Text("create profile"),
-              onTap: () {
-                print("nav to create profile page");
-              },
+          ),
+          FlatButton(
+            onPressed: () {
+              var result =
+                  UserLocalDataSource().storePreferredLanguage("english");
+              print("$result");
+            },
+            color: Colors.orange,
+            child: Text(
+              "store language",
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
-            ListTile(
-              title: Text("feat-profile creation"),
-              onTap: () {
-                print("nav to workplace id page");
-              },
+          ),
+          FlatButton(
+            onPressed: () {
+              var result = UserLocalDataSource().getPreferredLanguage();
+              print("$result");
+            },
+            color: Colors.orange,
+            child: Text(
+              "fetch data storage",
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
-
-            ListTile(
-              title: Text("confirmation login_returning user"),
-              onTap: () {
-                print("nav to confirmation login_returning user page");
-              },
+          ),
+          FlatButton(
+            onPressed: () {
+              var result = UserLocalDataSource().storeUID("ddjd");
+              print("$result");
+            },
+            color: Colors.black,
+            child: Text(
+              "store language",
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
-            ListTile(
-              title: Text("home_first time user"),
-              onTap: () {
-                print("nav to home_first time user page");
-              },
+          ),
+          FlatButton(
+            onPressed: () {
+              var result = UserLocalDataSource().getUID();
+              print("$result");
+            },
+            color: Colors.black,
+            child: Text(
+              "fetch uid",
+              style: TextStyle(
+                color: Colors.white,
+              ),
             ),
-            ListTile(
-              title: Text("home_first no surveys"),
-              onTap: () {
-                print("nav to home_first no surveys page");
-              },
-            ),
-            ListTile(
-              title: Text("home_returning user"),
-              onTap: () {
-                print("nav to home_returning user page");
-              },
-            ),
-            ListTile(
-              title: Text("profile screen"),
-              onTap: () {
-                print("nav to profile screen page");
-              },
-            ),
-            ListTile(
-              title: Text("profile screen #5"),
-              onTap: () {
-                print("nav to profile screen #5 page");
-              },
-            ),
-            ListTile(
-              title: Text("survey language"),
-              onTap: () {
-                print("nav to survey language page");
-              },
-            ),
-            ListTile(
-              title: Text("survey information"),
-              onTap: () {
-                print("nav to survey information page");
-              },
-            ),
-            ListTile(
-              title: Text("survey Q1"),
-              onTap: () {
-                print("nav to survey Q1 page");
-              },
-            ),
-            ListTile(
-              title: Text("survey1 thank you"),
-              onTap: () {
-                print("nav to survey1 thank you page");
-              },
-            ),
-            ListTile(
-              title: Text("thank you screen retr=u"),
-              onTap: () {
-                print("nav to _ page");
-              },
-            ),
-            ListTile(
-              title: Text("about - before user fill survey"),
-              onTap: () {
-                print("nav to _ page");
-              },
-            ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
