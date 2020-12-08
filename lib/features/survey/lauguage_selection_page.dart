b4ec3cb24701b9a2da12672956c6e6bfbfcdabe1
Souvik <souvik@pop-os.localdomain>
2020-12-08 17:39:34 +0530

    Changed the look of about page.

--- lib/features/survey/lauguage_selection_page.dart
+++ lib/features/survey/lauguage_selection_page.dart
@@ -22,10 +22,14 @@ class _LanguageSelectionPageState extends State<LanguageSelectionPage> {
         children: [
           Expanded(
             child: Center(
-
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text(S.of(context).languageSelectionPageCurrentlyEnglishIsTheOnlyLanguageAvailable, textAlign: TextAlign.center,),
+                child: Text(
+                  S
+                      .of(context)
+                      .languageSelectionPageCurrentlyEnglishIsTheOnlyLanguageAvailable,
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
           ),
@@ -34,25 +38,65 @@ class _LanguageSelectionPageState extends State<LanguageSelectionPage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                RaisedButton(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(S.of(context).back),
+                FlatButton(
+                  padding: EdgeInsets.fromLTRB(8.0, 8.0, 12.0, 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(
+                        Icons.chevron_left,
+                        size: 30,
+                      ),
+                      Text(
+                        S.of(context).back,
+                        style: TextStyle(
+                          fontSize: 22,
+                        ),
+                      ),
+                    ],
+                  ),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      width: 2,
+                      color: Colors.blue,
+                    ),
+                    borderRadius: BorderRadius.circular(
+                      7,
+                    ),
                   ),
                   onPressed: () {
                     Navigator.pop(context);
                   },
                 ),
-                RaisedButton(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(S.of(context).next),
+                FlatButton(
+                  padding: EdgeInsets.fromLTRB(12.0, 8.0, 8.0, 8.0),
+                  color: Colors.blue,
+                  child: Row(
+                    children: [
+                      Text(
+                        S.of(context).next,
+                        style: TextStyle(
+                          // color: Colors.white,
+                          fontSize: 22,
+                        ),
+                      ),
+                      Icon(
+                        Icons.chevron_right,
+                        size: 30,
+                      )
+                    ],
                   ),
                   onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
                         builder: (context) =>
-                            SurveyInformationPage(widget.surveyId)));
+                            SurveyInformationPage(widget.surveyId),
+                      ),
+                    );
                   },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(7),
+                  ),
                 ),
               ],
             ),
