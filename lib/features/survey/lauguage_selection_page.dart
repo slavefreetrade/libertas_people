c9dc0bade15fb743149438e633b080fa627e54f1
Michael Malak <michael.k.malak@gmail.com>
2021-02-02 00:48:23 +0200

    fix: language selection page

--- lib/features/survey/lauguage_selection_page.dart
+++ lib/features/survey/lauguage_selection_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:libertaspeople/features/survey/survey_information_page.dart';
 import 'package:libertaspeople/generated/l10n.dart';
+import 'package:libertaspeople/shared_ui_elements/shared_ui_elements.dart';
 
 class LanguageSelectionPage extends StatefulWidget {
   final String surveyId;
@@ -22,36 +23,43 @@ class _LanguageSelectionPageState extends State<LanguageSelectionPage> {
         children: [
           Expanded(
             child: Center(
-
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text(S.of(context).languageSelectionPageCurrentlyEnglishIsTheOnlyLanguageAvailable, textAlign: TextAlign.center,),
+                child: Text(
+                  S
+                      .of(context)
+                      .languageSelectionPageCurrentlyEnglishIsTheOnlyLanguageAvailable,
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.fromLTRB(48, 16, 48, 16),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                RaisedButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(S.of(context).back),
+                Expanded(
+                  child: ButtonBorderedWithBackArrow(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    previousButtonLabel: S.of(context).back,
                   ),
                 ),
-                RaisedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) =>
-                            SurveyInformationPage(widget.surveyId)));
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(S.of(context).next),
+                const SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  child: ButtonFullColorWithNextArrow(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              SurveyInformationPage(widget.surveyId),
+                        ),
+                      );
+                    },
+                    isFinalQuestion: false,
                   ),
                 ),
               ],
