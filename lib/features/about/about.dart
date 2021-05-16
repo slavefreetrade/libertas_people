69a2ef1e32a2a40c34f558bfa1f9619a043f2937
Michael Malak <michael.k.malak@gmail.com>
2021-05-16 16:33:02 +0200

    fix(about): adjust about ui

--- lib/features/about/about.dart
+++ lib/features/about/about.dart
@@ -46,139 +46,134 @@ class _AboutPageState extends State<AboutPage> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 16.0, left: 25, right: 25),
-          child: Column(
+      body: ListView(
+        padding: const EdgeInsets.only(top: 16.0, left: 25, right: 25),
+        children: [
+          Text(
+            S
+                .of(context)
+                .theLibertasPeopleAppIsHowWeDeliverHumanRightsQuestionsToYouInASecure,
+            style: const TextStyle(
+                fontWeight: FontWeight.w400, fontSize: 17.0, wordSpacing: 3.5),
+          ),
+          const SizedBox(height: 11),
+          Text(
+              S
+                  .of(context)
+                  .ourSurveyIsBasedOnThe10HumanRightPrinciplesOnInternationalLaw,
+              style: const TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 17.0,
+                  wordSpacing: 3.5)),
+          const SizedBox(height: 14),
+          Row(
             children: [
-              Text(
-                S
-                    .of(context)
-                    .theLibertasPeopleAppIsHowWeDeliverHumanRightsQuestionsToYouInASecure,
-                style: const TextStyle(
+              Image.asset(
+                AppImages.logoImage,
+                height: 48.88,
+                width: 47,
+              ),
+              Flexible(
+                child: Text(
+                  S.of(context).readMoreAboutSlavefreetrade,
+                  style: const TextStyle(
                     fontWeight: FontWeight.w400,
                     fontSize: 17.0,
-                    wordSpacing: 3.5),
-              ),
-              const SizedBox(height: 11),
-              Text(
-                  S
-                      .of(context)
-                      .ourSurveyIsBasedOnThe10HumanRightPrinciplesOnInternationalLaw,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 17.0,
-                      wordSpacing: 3.5)),
-              const SizedBox(height: 14),
-              Row(
-                children: [
-                  Image.asset(
-                    AppImages.logoImage,
-                    height: 48.88,
-                    width: 47,
+                    wordSpacing: 3.5,
                   ),
-                  Flexible(
-                    child: Text(
-                      S.of(context).readMoreAboutSlavefreetrade,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 17.0,
-                        wordSpacing: 3.5,
-                      ),
-                      softWrap: true,
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 10),
-              Text(S.of(context).tapOnEachButtonToReadMoreAboutEachPrinciple,
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w400,
-                      fontSize: 17.0,
-                      wordSpacing: 3.5)),
-              const SizedBox(height: 14),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: GridView.count(
-                  primary: false,
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  padding: const EdgeInsets.all(10),
-                  mainAxisSpacing: 12,
-                  crossAxisSpacing: 12,
-                  crossAxisCount: 2,
-                  childAspectRatio: 5 / 2,
-                  children: getAboutData(context).map((principle) {
-                    return TextButton(
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        )),
-                        backgroundColor: MaterialStateProperty.all(
-                          Color(principle['color'] as int),
-                        ),
-                        textStyle: MaterialStateProperty.all(
-                          const TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (BuildContext context) {
-                          return Principle(
-                            logo: principle['logo'] as String,
-                            title: principle['title'] as String,
-                            text: principle['text'] as String,
-                          );
-                        }));
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                        child: Row(
-                          children: [
-                            Text(
-                              principle['id'] as String,
-                              style: const TextStyle(
-                                  color: AppColors.white,
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.w300),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                left: 8.0,
-                                right: 8.0,
-                              ),
-                              child: Image.asset(
-                                principle['image'] as String,
-                                width: principle['image_size'] as double,
-                              ),
-                            ),
-                            Flexible(
-                              child: Text(
-                                principle['title'].substring(
-                                    0, principle['title'].length - 1) as String,
-                                style: TextStyle(
-                                  color: AppColors.white,
-                                  fontSize: principle['size'] as double,
-                                ),
-                                softWrap: true,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  }).toList(),
+                  softWrap: true,
                 ),
-              ),
-              const SizedBox(height: 30),
-              ButtonOrangeColor(
-                  onPressed: widget.onTakeSurveyPressed,
-                  label: S.of(context).takeSurvey,
-                ),
-              const SizedBox(height: 30),
+              )
             ],
           ),
-        ),
+          const SizedBox(height: 10),
+          Text(S.of(context).tapOnEachButtonToReadMoreAboutEachPrinciple,
+              style: const TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 17.0,
+                  wordSpacing: 3.5)),
+          const SizedBox(height: 14),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child: GridView.count(
+              primary: false,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              padding: const EdgeInsets.all(10),
+              mainAxisSpacing: 12,
+              crossAxisSpacing: 12,
+              crossAxisCount: 2,
+              childAspectRatio: 1.8,
+              children: getAboutData(context).map((principle) {
+                return TextButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )),
+                    backgroundColor: MaterialStateProperty.all(
+                      Color(principle['color'] as int),
+                    ),
+                    textStyle: MaterialStateProperty.all(
+                      const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (BuildContext context) {
+                      return Principle(
+                        logo: principle['logo'] as String,
+                        title: principle['title'] as String,
+                        text: principle['text'] as String,
+                      );
+                    }));
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          principle['id'] as String,
+                          style: const TextStyle(
+                              color: AppColors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w300),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 8.0,
+                            right: 8.0,
+                          ),
+                          child: Image.asset(
+                            principle['image'] as String,
+                            width: principle['image_size'] as double,
+                          ),
+                        ),
+                        Flexible(
+                          child: Text(
+                            principle['title'].substring(
+                                0, principle['title'].length - 1) as String,
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: AppColors.white,
+                              fontSize: principle['size'] as double,
+                            ),
+                            softWrap: true,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+          const SizedBox(height: 30),
+          ButtonOrangeColor(
+            onPressed: widget.onTakeSurveyPressed,
+            label: S.of(context).takeSurvey,
+          ),
+          const SizedBox(height: 30),
+        ],
       ),
     );
   }
--- lib/features/about/principle.dart
+++ lib/features/about/principle.dart
@@ -13,54 +13,49 @@ class Principle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
-    final double height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: Text(S.of(context).principle),
       ),
-      body: Container(
-        width: width,
-        height: height,
-        margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
-        child: Column(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: Image(
-                    image: AssetImage(logo),
-                  ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+        children: [
+          Row(
+            children: [
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Image(
+                  image: AssetImage(logo),
                 ),
-                const SizedBox(
-                  width: 20,
-                ),
-                SizedBox(
-                  width: width * 0.6,
-                  child: Text(
-                    title,
-                    style: const TextStyle(
-                      fontSize: 26.5,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
-              child: Text(
-                text,
-                style: const TextStyle(
-                    fontSize: 17,
-                    fontFamily: AppFonts.helveticaNeue,
-                    fontWeight: FontWeight.w400),
               ),
+              const SizedBox(
+                width: 20,
+              ),
+              SizedBox(
+                width: width * 0.6,
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+            child: Text(
+              text,
+              textAlign: TextAlign.justify,
+              style: const TextStyle(
+                  fontSize: 17,
+                  fontFamily: AppFonts.helveticaNeue,
+                  fontWeight: FontWeight.w400),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
